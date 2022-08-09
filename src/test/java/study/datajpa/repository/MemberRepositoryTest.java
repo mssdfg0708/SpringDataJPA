81d64478df9a9c870074e773de8869614f765a11
mssdfg0708 <mssdfg0708@naver.com>
2022-08-09 22:58:52 +0900

    확장 기능

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -180,4 +180,9 @@ class MemberRepositoryTest {
 
         em.flush();
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
